7de69c3beac33df5f7f07894eb4ec903e40140aa
RuanVianaa <ruanpabloviana12@gmail.com>
2026-02-23 23:46:44 -0300

    Atualização do jogo: sistema de erros e reinício

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
@@ -9,51 +10,67 @@ public class Main {
         ArrayList<Pergunta> perguntas = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
-        Pergunta p1 = new Pergunta(
+        // PERGUNTAS
+        perguntas.add(new Pergunta(
                 "Quanto tempo a Terra demora para dar uma volta completa em torno dela mesma?",
                 "24 horas",
                 "365 dias",
                 "30 dias",
                 "12 horas",
-                'a');
+                'a'));
 
-        perguntas.add(p1);
-        
-        Pergunta p2 = new Pergunta(
+        perguntas.add(new Pergunta(
                 "Quem inventou a lâmpada?",
                 "Nikola Tesla",
                 "Steve Jobs",
                 "Kanye West",
                 "Thomas Edison",
-                'd');
+                'd'));
 
-        perguntas.add(p2);
-        
-        Pergunta p3 = new Pergunta(
+        perguntas.add(new Pergunta(
                 "Qual é o filme de maior bilheteria de todos os tempos?",
                 "Moana",
                 "Avatar",
                 "Vingadores: Ultimato",
                 "Titanic",
-                'b');
-        
-        perguntas.add(p3);
+                'b'));
 
-        Pergunta p4 = new Pergunta(
+        perguntas.add(new Pergunta(
                 "Quem é a pessoa mais rica de todos os tempos, com uma fortuna imensurável?",
                 "Kim Kardashian",
                 "Elon Musk",
-                "Stive Jobs",
+                "Steve Jobs",
                 "Mansa Musa",
-                'd');
+                'd'));
 
-        perguntas.add(p4);
+        perguntas.add(new Pergunta(
+                "Qual é considerado pela maior parte da crítica o melhor álbum do século XXI?",
+                "Kid A - Radiohead",
+                "To Pimp a Butterfly - Kendrick Lamar",
+                "Lemonade - Beyoncé",
+                "My Beautiful Dark Twisted Fantasy - Kanye West",
+                'c'));
+
+        perguntas.add(new Pergunta(
+                "Qual é o nome do maior deserto do mundo?",
+                "Deserto do Saara",
+                "Deserto de Gobi",
+                "Deserto da Antártida",
+                "Deserto de Kalahari",
+                'c'));
+
+        // Embaralha as perguntas para que a ordem seja diferente a cada jogo
+        Collections.shuffle(perguntas);
+
         int indice = 0;
         int erros = 0;
+        int acertos = 0;
+
         while (indice < perguntas.size()) {
 
             Pergunta atual = perguntas.get(indice);
 
+            System.out.println("\nPergunta " + (indice + 1));
             System.out.println(atual.pergunta);
             System.out.println("a) " + atual.alternativaA);
             System.out.println("b) " + atual.alternativaB);
@@ -63,19 +80,31 @@ public class Main {
             System.out.print("Digite sua resposta: ");
             char resposta = sc.next().toLowerCase().charAt(0);
 
+            // Verifica se a resposta é correta
             if (resposta == atual.respostaCorreta) {
                 System.out.println("Acertou!");
                 indice++;
+                acertos++;
             } else {
-                System.out.println("Errou!");
                 erros++;
-
+                System.out.println("Errou!");
+                System.out.println("Erros: " + erros + "/3");
+            
+            // Se o jogador errar 3 vezes, o jogo é reiniciado
+            
                 if (erros == 3) {
-                    System.out.println("Você errou 3 vezes. O jogo será reiniciado.");
+                    System.out.println("\nVocê errou 3 vezes. O jogo será reiniciado!");
                     indice = 0;
                     erros = 0;
+                    acertos = 0;
+                    Collections.shuffle(perguntas);
                 }
             }
         }
+
+        System.out.println("\nParabéns! Você zerou o jogo!");
+        System.out.println("Total de acertos: " + acertos);
+
+        sc.close();
     }
 }
--- Pergunta.java
+++ Pergunta.java
@@ -6,7 +6,8 @@ public class Pergunta {
     String alternativaD;
     char respostaCorreta;
 
-    public Pergunta(String pergunta, String alternativaA, String alternativaB, String alternativaC, String alternativaD, char respostaCorreta) {
+    public Pergunta(String pergunta, String alternativaA, String alternativaB,
+                    String alternativaC, String alternativaD, char respostaCorreta) {
         this.pergunta = pergunta;
         this.alternativaA = alternativaA;
         this.alternativaB = alternativaB;
